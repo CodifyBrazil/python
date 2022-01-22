094c0b4f3e8a94b449300ae7247dd463fb94ef88
rafaelguilber <rafaelguilberdosreis@hotmail.com>
2022-01-22 11:15:47 -0300

    inicio

---
+++ #/Banco_ap/classe.py
@@ -0,0 +1,18 @@
+class banco:
+
+#Nome do nosso banco = Direct Bank
+#Agencia padrao Direct Bank = 00001
+
+    def __init__(self, nome, cpf):
+        '''
+        :param agencia: Envia agencia do cliente
+        :param conta: envia conta corrente do cliente
+        '''
+
+        self.nome = nome
+        self.cpf = cpf
+        self.saldo = 0
+
+        def criar_conta(self, nome, cpf):
+
+
---
+++ #/Banco_ap/main.py
@@ -0,0 +1,24 @@
+import classe
+
+
+clientes = {'123.456.789-00':
+        {'Id_banco': 'Itau', 'Agencia': '0123', 'Conta': '0458', 'Cliente': 'Julio Almeida', 'Saldo': 0},
+    '987.654.321-00':
+    {'Id_banco': 'Caixa Economica', 'Agencia': '0154', 'Conta': '0148', 'Cliente': 'Fausto Alencar', 'Saldo': 0},
+    '123.456.789-01':
+    {'Id_banco': 'Santander', 'Agencia': '0164', 'Conta': '0146', 'Cliente': 'Bruno Souza', 'Saldo': 0},
+    '123.45.789-02':
+    {'Id_banco': 'NuBank', 'Agencia': '0648', 'Conta': '0163', 'Cliente': 'Camila Alves', 'Saldo': 0},
+            '123.456.789-03':
+    {'Id_banco': 'Banco do Brasil', 'Agencia': '0184', 'Conta': '0954', 'Cliente': 'Gisele Suzano', 'Saldo': 0},
+}
+
+tranferencias = {
+    'CPF':{
+        'CPF_Origem': {'Id_banco': 'Itau', 'Valor': 'valor', 'data/hora': 'data/hora'},
+        'CPF_destino': {'Id_banco': 'Itau', 'Valor': 'valor', 'data/hora': 'data/hora'}
+        }
+}
+
+
+
