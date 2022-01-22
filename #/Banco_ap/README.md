5d57a2101cdfad40f3ffeeb3ccffd5477e7c7923
rafaelguilber <rafaelguilberdosreis@hotmail.com>
2022-01-22 11:23:02 -0300

    inicio

---
+++ #/Banco_ap/README.md
@@ -0,0 +1,5 @@
+#Estamos criando um sistema bancario
+
+Apenas para aprendizado, não havera valores reais de nenhum tipo de dinheiro
+
+Fique a vontade para comentar, isto é apenas para aprendizado.
--- #/Banco_ap/classe.py
+++ #/Banco_ap/classe.py
@@ -1,18 +1,24 @@
-class banco:
+
+
+
+class Banco:
 
 #Nome do nosso banco = Direct Bank
 #Agencia padrao Direct Bank = 00001
 
     def __init__(self, nome, cpf):
         '''
-        :param agencia: Envia agencia do cliente
-        :param conta: envia conta corrente do cliente
+        :param nome: recebe nome do cliente
+        :param cpf: recebe o cpf
         '''
 
         self.nome = nome
         self.cpf = cpf
+        self.agencia = 00001
+        self.conta_corrente = 0
         self.saldo = 0
 
         def criar_conta(self, nome, cpf):
+
 
 
